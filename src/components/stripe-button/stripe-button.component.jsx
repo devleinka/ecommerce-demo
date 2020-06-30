1188737ba59341911eb3712682965dac4a3a6ce4
devleinka <42700247+devleinka@users.noreply.github.com>
2020-06-30 16:55:27 +0200

    updated stripe integration and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51Gzk4hIBG9JFjG9QFCpgWcpkQZbp7by0mhz9rUMBdfUUlzNbWQvjdsiBHNRI4KgGo0m1nwBzNr5MxkERiVg20QwD00ENaJDExT';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+        label='Pay Now' 
+        name= 'Fab Snail Fashion' 
+        billingAddress 
+        shippingAddress 
+        image='https://svgshare.com/i/CUz.svg' 
+        description={`Your total is $${price}`} 
+        amount={priceForStripe} 
+        panelLabel='Pay Now' 
+        token={onToken} 
+        stripeKey={publishableKey}
+        />
+    )
+};
+
+export default StripeCheckoutButton;
