2e52db171df453d453081e3a9429d1e1d861e983
Lea Mikulcová <42700247+devleinka@users.noreply.github.com>
2020-08-05 23:06:28 +0200

    added performance optimizations with react lazy, suspense and memo

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,28 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+import Spinner from '../../components/spinner/spinner.component';
+import ErrorBoundary from '../../components/error-boundary/error-boundary.component';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
+const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'));
+const CollectionPageContainer = lazy(() => import('../collection/collection.container'))
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
     useEffect(() => {fetchCollectionsStart();},[fetchCollectionsStart])
 
     return (
         <div className='shop-page' >
-            < Route 
-                exact 
-                path={`${match.path}`} 
-                component={CollectionsOverviewContainer} 
-            />
-            < Route 
-                path={`${match.path}/:collectionId`} 
-                component={CollectionPageContainer} 
-            />
+            <ErrorBoundary>
+                <Suspense fallback={<Spinner />} >
+                    < Route 
+                        exact 
+                        path={`${match.path}`} 
+                        component={CollectionsOverviewContainer} 
+                    />
+                    < Route 
+                        path={`${match.path}/:collectionId`} 
+                        component={CollectionPageContainer} 
+                    />
+                </Suspense>
+            </ErrorBoundary>
         </div>
     )
 }
